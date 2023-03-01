6b3b7b4628db7c8310b040136c6309a9e4c6d3af
sindor <sindorjunior@gmail.com>
2023-03-01 08:36:04 -0300

    Início do Projeto

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <h1>Problema de Monty Hall</h1>
+        <Gift />
+    </div>
+  
+</template>
+
+<script>
+import Gift from './components/Gift.vue'
+
+export default {
+    name: 'App',
+    components: { Gift }
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+
+body {
+    color: #FFF;
+    background: linear-gradient(to right, rgb(21, 153, 8), rgb(21, 87, 153));
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App'
+
+createApp(App).mount('#app')
+/*import Vue from 'vue'
+import App from './App'
+
+new Vue({
+    render: h => h(App)
+}).$mount("#app")*/
